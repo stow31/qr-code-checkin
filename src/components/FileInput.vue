b02ad6ed5a958b0169bb1a87befebea92a2fe725
Sophie Townsend <se.townsend31@gmail.com>
2024-08-17 15:41:38 +1000

    add nav and file input

---
+++ src/components/FileInput.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+    <input type="file" class="file-input file-input-bordered w-full max-w-xs" />
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,29 @@ export default {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+            "default": "#c9c9c9",
+            "primary": "#00380e",
+            "secondary": "#618c6c",
+            "accent": "#f4f5d5",
+            "neutral": "#c4c4ad",
+            "base-100": "#ffffff",
+            "info": "#9d9df2",
+            "success": "#d7fcd7",
+            "warning": "#f5d8b0",
+            "error": "#f5babb",
+          },
+        },
+      ],
+    },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
